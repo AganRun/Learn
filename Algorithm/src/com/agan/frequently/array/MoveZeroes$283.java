a7641060089f3cbcc299461f7bc82be8692ac937
AganRun <1379154067@qq.com>
2022-03-21 08:27:42 +0800

    feat(Algorithm) 快慢指针，同26题移除元素

---
+++ Algorithm/src/com/agan/frequently/array/MoveZeroes$283.java
@@ -0,0 +1,45 @@
+package com.agan.frequently.array;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ *
+ * 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/move-zeroes
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MoveZeroes$283 {
+
+    /**
+     * 快慢指针，同26题移除元素
+     * @param nums
+     */
+    public void moveZeroes(int[] nums) {
+        int slowIndex = 0;
+        for (int fastIndex = 0; fastIndex < nums.length; fastIndex++) {
+            if (nums[fastIndex] != 0) {
+                nums[slowIndex++] = nums[fastIndex];
+            }
+        }
+        while (slowIndex < nums.length) {
+            nums[slowIndex++] = 0;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void main(String[] args) {
+        MoveZeroes$283 m = new MoveZeroes$283();
+        m.moveZeroes(new int[]{0,1,0,3,12});
+    }
+
+}
