04f97798eebb332547cf1508ea1d5608f7cdae71
AganRun <1379154067@qq.com>
2023-06-03 07:27:12 +0800

    feat(数组) 27，快慢指针。独立做出

---
+++ Algorithm/src/com/agan/leetcode/array/$27_RemoveElement.java
@@ -0,0 +1,39 @@
+package com.agan.leetcode.array;
+
+/**
+ * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+ *
+ * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+ *
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/remove-element
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * 输入：nums = [3,2,2,3], val = 3
+ * 输出：2, nums = [2,2]
+ * 解释：函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。你不需要考虑数组中超出新长度后面的元素。例如，函数返回的新长度为 2 ，而 nums = [2,2,3,3] 或 nums = [2,2,0,0]，也会被视作正确答案。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/remove-element
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class $27_RemoveElement {
+
+    public int removeElement(int[] nums, int val) {
+        int slowIndex = 0;
+        for (int fastIndex = 0; fastIndex < nums.length; fastIndex++) {
+            if (nums[fastIndex] != val) {
+                nums[slowIndex++] = nums[fastIndex];
+            }
+        }
+        return slowIndex;
+    }
+
+    public static void main(String[] args) {
+        $27_RemoveElement re = new $27_RemoveElement();
+        re.removeElement(new int[]{2,3,3,2}, 3);
+    }
+
+}
