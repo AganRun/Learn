9f85b6210fdcd878b01d82556efc983ecf97a041
AganRun <1379154067@qq.com>
2023-06-07 09:58:02 +0800

    feat(hash) 1 两数之和

---
+++ Algorithm/src/com/agan/leetcode/hash/$1_TwoSum.java
@@ -0,0 +1,37 @@
+package com.agan.leetcode.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ *
+ * 你可以按任意顺序返回答案。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class $1_TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
