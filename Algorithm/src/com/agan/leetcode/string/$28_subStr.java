60a0641c59abe41d8990b263b1add8615e70cc76
AganRun <1379154067@qq.com>
2023-06-12 23:43:20 +0800

    feat(string) 28 KMP算法

---
+++ Algorithm/src/com/agan/leetcode/string/$28_subStr.java
@@ -0,0 +1,100 @@
+package com.agan.leetcode.string;
+
+import java.util.Arrays;
+
+/**
+ * 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串的第一个匹配项的下标（下标从 0 开始）。如果 needle 不是 haystack 的一部分，则返回  -1 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：haystack = "sadbutsad", needle = "sad"
+ * 输出：0
+ * 解释："sad" 在下标 0 和 6 处匹配。
+ * 第一个匹配项的下标是 0 ，所以返回 0 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class $28_subStr {
+
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) return 0;
+        int[] next = new int[needle.length()];
+        getNext(next, needle);
+
+        int j = 0;
+        for (int i = 0; i < haystack.length(); i++) {
+            while (j > 0 && needle.charAt(j) != haystack.charAt(i))
+                j = next[j - 1];
+            if (needle.charAt(j) == haystack.charAt(i))
+                j++;
+            if (j == needle.length())
+                return i - needle.length() + 1;
+        }
+        return -1;
+    }
+
+    private void getNext(int[] next, String s) {
+        int j = 0;
+        next[0] = 0;
+        for (int i = 1; i < s.length(); i++) {
+            while (j > 0 && s.charAt(j) != s.charAt(i))
+                j = next[j - 1];
+            if (s.charAt(j) == s.charAt(i))
+                j++;
+            next[i] = j;
+        }
+    }
+
+    /**
+     * =============================接下来自己做=========================
+     * @param args
+     */
+
+    public int strStr2(String haystack, String needle) {
+        if (haystack.isEmpty()) {
+            return -1;
+        }
+        int[] next = new int[needle.length()];
+        getNextM(next, needle);
+        int j = 0;
+        for (int i = 0; i < haystack.length(); i++) {
+            while (j > 0 && haystack.charAt(i) != needle.charAt(j)) {
+                j = next[j - 1];
+            }
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                j++;
+            }
+            if (j == needle.length()) {
+                return i - needle.length() + 1;
+            }
+        }
+        return -1;
+
+    }
+    public void getNextM(int[] next, String str) {
+        int j = 0;
+        for (int i = 1; i < str.length(); i++) {
+            while (j > 0 && str.charAt(i) != str.charAt(j)) {
+                j = next[j - 1];
+            }
+            if (str.charAt(i) == str.charAt(j)) {
+                j++;
+            }
+            next[i] = j;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        $28_subStr subStr = new $28_subStr();
+//        System.out.println(subStr.strStr("ababxababf", "ababf"));
+        int[] next = new int[5];
+        subStr.getNextM(next, "ababf");
+        System.out.println(Arrays.toString(next));
+        System.out.println(subStr.strStr2("ababxababf", "ababf"));
+    }
+}
