3ebfac43eba29b7ff906746c44b1801be6cfb207
AganRun <1379154067@qq.com>
2023-06-29 08:31:25 +0800

    feat(144) 二叉树前序遍历

---
+++ Algorithm/src/com/agan/leetcode/tree/$144_BinaryTreePreorderTraversal.java
@@ -0,0 +1,51 @@
+package com.agan.leetcode.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+ */
+public class $144_BinaryTreePreorderTraversal {
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(){}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        traversal(root, result);
+        return result;
+    }
+
+    public void traversal(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        result.add(root.val);
+        traversal(root.left, result);
+        traversal(root.right, result);
+    }
+
+
+    public static void main(String[] args) {
+        $144_BinaryTreePreorderTraversal traversal = new $144_BinaryTreePreorderTraversal();
+        TreeNode left = new TreeNode(2, null, null);
+        TreeNode right = new TreeNode(3, null, null);
+        TreeNode root = new TreeNode(1, left, right);
+        System.out.println(traversal.preorderTraversal(root));
+    }
+
+}
