fa76b31d36d382958fe65bfd03498d173f63f5f6
AganRun <1379154067@qq.com>
2023-07-12 09:22:06 +0800

    feat(tree) 701 二叉搜索树的插入操作

---
+++ Algorithm/src/com/agan/leetcode/tree/$701_InsertIntoABinarySearchTree.java
@@ -0,0 +1,34 @@
+package com.agan.leetcode.tree;
+
+/**
+ * 给定二叉搜索树（BST）的根节点 root 和要插入树中的值 value ，将值插入二叉搜索树。 返回插入后二叉搜索树的根节点。 输入数据 保证 ，新值和原始二叉搜索树中的任意节点值都不同。
+ *
+ * 注意，可能存在多种有效的插入方式，只要树在插入后仍保持为二叉搜索树即可。 你可以返回 任意有效的结果 。
+ *
+ *  
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/insert-into-a-binary-search-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class $701_InsertIntoABinarySearchTree {
+
+    /**
+     * 返回任意满足条件的都行。不要求平衡
+     * 那么直接寻找到树的叶子节点的null.然后加进去即可
+     * @param root
+     * @param val
+     * @return
+     */
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (root.val > val) {
+            root.left = insertIntoBST(root.left, val);
+        } else if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+}
