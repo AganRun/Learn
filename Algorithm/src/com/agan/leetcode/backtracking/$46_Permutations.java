279f9c2729ec7ab10a6f047b2b1baedb6305d25d
AganRun <1379154067@qq.com>
2023-07-16 21:54:11 +0800

    feat(tree) 46 全排列

---
+++ Algorithm/src/com/agan/leetcode/backtracking/$46_Permutations.java
@@ -0,0 +1,62 @@
+package com.agan.leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ * 示例 2：
+ *
+ * 输入：nums = [0,1]
+ * 输出：[[0,1],[1,0]]
+ * 示例 3：
+ *
+ * 输入：nums = [1]
+ * 输出：[[1]]
+ *  
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/permutations
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class $46_Permutations {
+
+    List<List<Integer>> result = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+    boolean[] used;
+    public List<List<Integer>> permute(int[] nums) {
+        used = new boolean[nums.length];
+        backtracking(nums);
+        return result;
+    }
+
+    public void backtracking(int[] nums) {
+        if (path.size() == nums.length) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            used[i] = true;
+            path.add(nums[i]);
+            backtracking(nums);
+            path.removeLast();
+            used[i] = false;
+        }
+    }
+
+    public static void main(String[] args) {
+        $46_Permutations s = new $46_Permutations();
+        System.out.println(s.permute(new int[]{1,2,3}));
+    }
+}
